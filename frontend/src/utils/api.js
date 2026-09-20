9f4a4f7e1286799f192bc3ad452a67d61652ed5e
gousiya02 <gousiyarbadni@gmail.com>
2026-09-20 16:04:33 +0530

    Support VITE_API_BASE_URL for flexible production deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getAuthToken } from './auth'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL || '/api' })
 
 api.interceptors.request.use((config) => {
   const token = getAuthToken()
